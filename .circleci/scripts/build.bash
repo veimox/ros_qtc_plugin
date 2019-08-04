e989484408751c6c8b509674134e8050cc984fe4
Jorge Rodriguez <veimox@gmail.com>
2019-08-04 15:01:51 +0200

    clonning into right folder

--- .circleci/scripts/build.bash
+++ .circleci/scripts/build.bash
@@ -2,14 +2,17 @@
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPT_PATH/utils.bash
 
-# Move the repo to the working folder
-mv $HOME/$CIRCLECI_PROJECT_REPONAME $BASE_PATH/ros_qtc_plugin
 cd $BASE_PATH
 
+pretty_header "Building"
+pretty_print "Creating build folder"
 mkdir ros_qtc_plugin-build
 cd $BASE_PATH/ros_qtc_plugin-build
+pretty_print "Running qmake"
 qmake ../ros_qtc_plugin/ros_qtc_plugin.pro -r 
+pretty_print "Making"
 make -j8$(nproc) || exit 1
 
 # Next change the rpath to use the local Qt Libraries copied into the Qt Creator Directory
+pretty_print "Update Rpath"
 chrpath -r \$\ORIGIN:\$\ORIGIN/..:\$\ORIGIN/../lib/qtcreator:\$\ORIGIN/../../Qt/lib $BASE_PATH/$INSTALL_DIR/lib/qtcreator/plugins/libROSProjectManager.so  
